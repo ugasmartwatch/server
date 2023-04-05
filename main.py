8ef7141f9dddc7000285f3e92dfd5594942e4899
Oracle Server <velaboratory@gmail.com>
2023-04-05 22:17:09 +0000

    some fixes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import pandas as pd
+import sqlite3
 
 app = FastAPI()
 
@@ -13,23 +15,31 @@ async def get_device_ids():
        return pd.read_sql("select device_id from profiles", con)['device_id'].to_list() 
     return {"message": "UGASmartwatch Database unavailable contact Kyle Johnsen"}
 
-@app.put("/profile")
-async def put_profile(device_id, profile):
+class Profile(BaseModel):
+    device_id: str
+    config_json: str
+
+@app.put("/profiles")
+async def put_profile(p: Profile):
     with dbConnection() as con:
-        con.execute("INSERT INTO profiles (device_id, config) values (?,?)", (device_id, profile));
+        con.execute("INSERT INTO profiles (device_id, config) values (?,?)", (p.device_id, p.config_json));
         return {"success": "True"}
     return {"success": "False", "message": "UGASmartwatch Database unavailable contact Kyle Johnsen"}
 
-@app.get("/profile")
+@app.get("/profiles")
 async def get_profile(device_id):
     with dbConnection() as con:
-       return pd.read_sql("select * from profiles where devicie_id = ?", con, (device_id,))['device_id'].to_list() 
+       return pd.read_sql("select * from profiles where device_id = ?", con, params=(device_id,)).iloc[0].to_json()
     return {"success": "False", "message": "UGASmartwatch Database unavailable contact Kyle Johnsen"}
 
-@app.put("/sync")
-async def put_sync(device_id, sync_data):
+class Sync(BaseModel):
+    device_id: str
+    sync_data: str
+
+@app.put("/syncs")
+async def put_sync(s: Sync):
     with dbConnection() as con:
-        con.execute("INSERT INTO syncs (device_id, data) values (?,?)", (device_id, sync_data));
+        con.execute("INSERT INTO syncs (device_id, data) values (?,?)", (s.device_id, s.sync_data));
         return {"success": "True"}
     return {"success": "False", "message": "UGASmartwatch Database unavailable contact Kyle Johnsen"}
 
